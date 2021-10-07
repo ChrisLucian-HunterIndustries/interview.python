9cde65ca51e75d237d126c15c811ca0543d2f2b5
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-10-07 16:02:16 -0600

    start

---
+++ phone_number.py
@@ -0,0 +1,14 @@
+class PhoneNumber:
+    def __init__(self, raw_text):      
+        pass
+    def getOriginalText(self) -> str:
+        pass
+
+    def getStrippedNumber(self) -> str:
+        pass
+
+    def getValueAsNorthAmerican(self) -> str:
+        pass
+
+    def getValueAsInternational(self) -> str :
+        pass
